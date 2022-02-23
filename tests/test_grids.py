642b5784fc22550a7a2b155adcb364ac8767232c
Matt Wheeler <matthew.t.wheeler@uea.ac.uk>
2022-02-23 14:19:01 +0000

    Added testing of meshgrid shapes

--- tests/test_grids.py
+++ tests/test_grids.py
@@ -4,6 +4,20 @@ import pygpe.shared.grid as grid
 
 class TestGrid(unittest.TestCase):
 
+    def test_correct_mesh_shape(self):
+        """
+        Tests to see whether meshgrids match the
+        shape of points passed in.
+        """
+
+        shape = (64, 64)
+        grid2d = grid.Grid2D(shape, (0.5, 0.5))
+
+        self.assertEqual(grid2d.x_mesh.shape, shape)
+        self.assertEqual(grid2d.y_mesh.shape, shape)
+        self.assertEqual(grid2d.fourier_x_mesh.shape, shape)
+        self.assertEqual(grid2d.fourier_y_mesh.shape, shape)
+
     def test_correct_fourier_shift(self):
         """Tests to see whether Fourier space
         meshes are correctly shifted to the center of
