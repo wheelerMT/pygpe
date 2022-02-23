5128cf0800789ff31be0adab17af8322838a7e2c
Matt Wheeler <matthew.t.wheeler@uea.ac.uk>
2022-02-23 17:52:24 +0000

    Fixed components not being cupy arrays in set_initial_state. Also added fft functions and tests

--- pygpe/spin_1/wavefunction.py
+++ pygpe/spin_1/wavefunction.py
@@ -16,8 +16,18 @@ class Wavefunction2D:
 
     def set_initial_state(self, ground_state: str):
         if ground_state.lower() == "polar":
-            self.plus_component = complex(0., 0.)
-            self.zero_component = complex(1., 0.)
-            self.minus_component = complex(0., 0.)
+            self.plus_component = cp.zeros((self.grid.num_points_x, self.grid.num_points_y), dtype='complex64')
+            self.zero_component = cp.ones((self.grid.num_points_x, self.grid.num_points_y), dtype='complex64')
+            self.minus_component = cp.zeros((self.grid.num_points_x, self.grid.num_points_y), dtype='complex64')
         else:
             raise ValueError(f"Argument \"{ground_state}\" is not a supported type")
+
+    def fft(self):
+        self.fourier_plus_component = cp.fft.fft2(self.plus_component)
+        self.fourier_zero_component = cp.fft.fft2(self.zero_component)
+        self.fourier_minus_component = cp.fft.fft2(self.minus_component)
+
+    def ifft(self):
+        self.plus_component = cp.fft.ifft2(self.fourier_plus_component)
+        self.zero_component = cp.fft.ifft2(self.fourier_zero_component)
+        self.minus_component = cp.fft.ifft2(self.fourier_minus_component)
--- tests/test_wavefunction_spin1.py
+++ tests/test_wavefunction_spin1.py
@@ -1,4 +1,5 @@
 import unittest
+import cupy as cp
 from pygpe.shared.grid import Grid2D
 from pygpe.spin_1.wavefunction import Wavefunction2D
 
@@ -10,12 +11,27 @@ class TestWavefunctionSpin1(unittest.TestCase):
         wavefunction = Wavefunction2D(grid)
         wavefunction.set_initial_state("Polar")
 
-        self.assertEqual(wavefunction.plus_component, complex(0., 0.))
-        self.assertEqual(wavefunction.zero_component, complex(1., 0.))
-        self.assertEqual(wavefunction.minus_component, complex(0., 0.))
+        self.assertEqual(wavefunction.plus_component.all(), 0.)
+        self.assertEqual(wavefunction.zero_component.all(), 1.)
+        self.assertEqual(wavefunction.minus_component.all(), 0.)
 
     def test_set_initial_state_raises_error(self):
         grid = Grid2D((64, 64), (0.5, 0.5))
         wavefunction = Wavefunction2D(grid)
         with self.assertRaises(ValueError):
             wavefunction.set_initial_state("garbage")
+
+    def test_fft_normalised(self):
+        grid = Grid2D((64, 64), (0.5, 0.5))
+        wavefunction_1 = Wavefunction2D(grid)
+        wavefunction_1.set_initial_state("polar")
+        wavefunction_2 = Wavefunction2D(grid)
+        wavefunction_2.set_initial_state("polar")
+
+        wavefunction_2.fft()
+        wavefunction_2.ifft()
+
+        # assert_array_equal returns None if arrays are equal
+        self.assertIsNone(cp.testing.assert_array_equal(wavefunction_1.plus_component, wavefunction_2.plus_component))
+        self.assertIsNone(cp.testing.assert_array_equal(wavefunction_1.zero_component, wavefunction_2.zero_component))
+        self.assertIsNone(cp.testing.assert_array_equal(wavefunction_1.minus_component, wavefunction_2.minus_component))
