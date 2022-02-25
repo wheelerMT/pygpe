560a7b6622aa6a545ba760c626e616e9bceb9b6b
Matt Wheeler <matthew.t.wheeler@uea.ac.uk>
2022-02-25 15:09:16 +0000

    Added test for checking grid lengths

--- tests/shared/test_grids.py
+++ tests/shared/test_grids.py
@@ -18,6 +18,15 @@ class TestGrid(unittest.TestCase):
         self.assertEqual(grid2d.fourier_x_mesh.shape, shape)
         self.assertEqual(grid2d.fourier_y_mesh.shape, shape)
 
+    def test_correct_lengths(self):
+        """Tests to see whether the length of each grid
+        dimension gives the expected length.
+        """
+
+        grid2d = grid.Grid2D((64, 64), (0.5, 0.5))
+        self.assertEqual(grid2d.length_x, 32.)
+        self.assertEqual(grid2d.length_y, 32.)
+
     def test_correct_fourier_shift(self):
         """Tests to see whether Fourier space
         meshes are correctly shifted to the center of
