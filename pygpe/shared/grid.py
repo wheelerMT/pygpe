22d93e786e8205fdfead474699286bc9c48eebd8
Matt Wheeler <matthew.t.wheeler@uea.ac.uk>
2022-02-28 13:42:41 +0000

    Changed construction of grid to be done on cpu

--- pygpe/shared/grid.py
+++ pygpe/shared/grid.py
@@ -1,4 +1,4 @@
-import cupy as cp
+import numpy as np
 from abc import ABC, abstractmethod
 from typing import Tuple
 
@@ -13,17 +13,17 @@ class Grid2D(Grid):
         self.grid_spacing_x, self.grid_spacing_y = grid_spacings
         self.length_x = self.num_points_x * self.grid_spacing_x
         self.length_y = self.num_points_y * self.grid_spacing_y
-        
-        x = cp.arange(-self.num_points_x // 2, self.num_points_x // 2) * self.grid_spacing_x
-        y = cp.arange(-self.num_points_y // 2, self.num_points_y // 2) * self.grid_spacing_y
-        self.x_mesh, self.y_mesh = cp.meshgrid(x, y)
+
+        x = np.arange(-self.num_points_x // 2, self.num_points_x // 2) * self.grid_spacing_x
+        y = np.arange(-self.num_points_y // 2, self.num_points_y // 2) * self.grid_spacing_y
+        self.x_mesh, self.y_mesh = np.meshgrid(x, y)
 
         # Generate Fourier space variables
-        self.fourier_spacing_x = cp.pi / (self.num_points_x // 2 * self.grid_spacing_x)
-        self.fourier_spacing_y = cp.pi / (self.num_points_y // 2 * self.grid_spacing_y)
+        self.fourier_spacing_x = np.pi / (self.num_points_x // 2 * self.grid_spacing_x)
+        self.fourier_spacing_y = np.pi / (self.num_points_y // 2 * self.grid_spacing_y)
 
-        fourier_x = cp.arange(-self.num_points_x // 2, self.num_points_x // 2) * self.fourier_spacing_x
-        fourier_y = cp.arange(-self.num_points_y // 2, self.num_points_y // 2) * self.fourier_spacing_y
-        self.fourier_x_mesh, self.fourier_y_mesh = cp.meshgrid(fourier_x, fourier_y)
-        self.fourier_x_mesh = cp.fft.fftshift(self.fourier_x_mesh)
-        self.fourier_y_mesh = cp.fft.fftshift(self.fourier_y_mesh)
+        fourier_x = np.arange(-self.num_points_x // 2, self.num_points_x // 2) * self.fourier_spacing_x
+        fourier_y = np.arange(-self.num_points_y // 2, self.num_points_y // 2) * self.fourier_spacing_y
+        self.fourier_x_mesh, self.fourier_y_mesh = np.meshgrid(fourier_x, fourier_y)
+        self.fourier_x_mesh = np.fft.fftshift(self.fourier_x_mesh)
+        self.fourier_y_mesh = np.fft.fftshift(self.fourier_y_mesh)
