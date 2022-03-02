e61c88d331302e39d6813bc0aa71e9339d81697f
Matt Wheeler <mattwheeler15@hotmail.co.uk>
2022-03-02 13:14:20 +0000

    Updated file to use new Wavefunction class

--- pygpe/spin_1/evolution.py
+++ pygpe/spin_1/evolution.py
@@ -1,15 +1,15 @@
 import cupy as cp
-from pygpe.spin_1.wavefunction import Wavefunction2D
+from pygpe.spin_1.wavefunction import Wavefunction
 
 
-def fft2(wavefunction: Wavefunction2D) -> None:
+def fft2(wavefunction: Wavefunction) -> None:
     """Performs a fast Fourier transform on each wavefunction component."""
     wavefunction.fourier_plus_component = cp.fft.fft2(wavefunction.plus_component)
     wavefunction.fourier_zero_component = cp.fft.fft2(wavefunction.zero_component)
     wavefunction.fourier_minus_component = cp.fft.fft2(wavefunction.minus_component)
 
 
-def ifft2(wavefunction: Wavefunction2D) -> None:
+def ifft2(wavefunction: Wavefunction) -> None:
     """Performs an inverse Fourier transform on each wavefunction component."""
     wavefunction.plus_component = cp.fft.ifft2(wavefunction.fourier_plus_component)
     wavefunction.zero_component = cp.fft.ifft2(wavefunction.fourier_zero_component)
