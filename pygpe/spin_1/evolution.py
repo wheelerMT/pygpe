7b74f09dfa78f62f655d129380aca3a6c2d11cb5
Matt Wheeler <mattwheeler15@hotmail.co.uk>
2022-03-02 17:22:36 +0000

    Switched to dimensionality agnostic FFT's. Updated tests appropriately

--- pygpe/spin_1/evolution.py
+++ pygpe/spin_1/evolution.py
@@ -2,15 +2,15 @@ import cupy as cp
 from pygpe.spin_1.wavefunction import Wavefunction
 
 
-def fft2(wavefunction: Wavefunction) -> None:
+def fft(wavefunction: Wavefunction) -> None:
     """Performs a fast Fourier transform on each wavefunction component."""
-    wavefunction.fourier_plus_component = cp.fft.fft2(wavefunction.plus_component)
-    wavefunction.fourier_zero_component = cp.fft.fft2(wavefunction.zero_component)
-    wavefunction.fourier_minus_component = cp.fft.fft2(wavefunction.minus_component)
+    wavefunction.fourier_plus_component = cp.fft.fftn(wavefunction.plus_component)
+    wavefunction.fourier_zero_component = cp.fft.fftn(wavefunction.zero_component)
+    wavefunction.fourier_minus_component = cp.fft.fftn(wavefunction.minus_component)
 
 
-def ifft2(wavefunction: Wavefunction) -> None:
+def ifft(wavefunction: Wavefunction) -> None:
     """Performs an inverse Fourier transform on each wavefunction component."""
-    wavefunction.plus_component = cp.fft.ifft2(wavefunction.fourier_plus_component)
-    wavefunction.zero_component = cp.fft.ifft2(wavefunction.fourier_zero_component)
-    wavefunction.minus_component = cp.fft.ifft2(wavefunction.fourier_minus_component)
+    wavefunction.plus_component = cp.fft.ifftn(wavefunction.fourier_plus_component)
+    wavefunction.zero_component = cp.fft.ifftn(wavefunction.fourier_zero_component)
+    wavefunction.minus_component = cp.fft.ifftn(wavefunction.fourier_minus_component)
