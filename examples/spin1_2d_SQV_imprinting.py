0bba6e1ebd9e5271e48b9a58bf0c580fb1321342
Matt Wheeler <mattwheeler15@hotmail.co.uk>
2022-07-04 16:04:58 +0100

    Fixed error in setting ground state and added plots of density and phase.

--- examples/spin1_2d_SQV_imprinting.py
+++ examples/spin1_2d_SQV_imprinting.py
@@ -1,5 +1,7 @@
 import pygpe.spin_1 as gpe
 import pygpe.shared.vortices as vort
+import matplotlib.pyplot as plt
+import cupy as cp
 
 # Generate grid object
 points = (512, 512)
@@ -13,6 +15,7 @@ params = {
     "p": 0.,
     "q": 0.,
     "trap": 0.,
+    "n0": 1,
 
     # Time params
     "dt": -1j * 1e-2,
@@ -22,10 +25,10 @@ params = {
 
 # Generate wavefunction object, set initial state and add noise
 psi = gpe.Wavefunction(grid)
-psi.set_ground_state("polar")
+psi.set_ground_state("polar", params)
 psi.add_noise_to_components("outer", 0., 1e-2)
 
-vort.add_singly_quantised_vortices(psi, 100, 1)  # Add SQVs to phase profile of wavefunction
+vort.add_singly_quantised_vortices(psi, 100, 1)  # Add 100 SQVs with a min distance of 1 to phase profile
 
 # Generate DataManager to store data for simulation
 data = gpe.DataManager(filename='test.hdf5', data_path='../data/')
@@ -42,3 +45,9 @@ for i in range(params["nt"]):
     gpe.renormalise_wavefunction(psi)
 
     data.save_wfn(psi)  # Save data
+
+# Plot density and phase of zero component
+fig, ax = plt.subplots(1, 2, figsize=(10, 6))
+ax[0].pcolormesh(cp.asnumpy(grid.x_mesh), cp.asnumpy(grid.y_mesh), abs(cp.asnumpy(psi.zero_component)) ** 2)
+ax[1].pcolormesh(cp.asnumpy(grid.x_mesh), cp.asnumpy(grid.y_mesh), cp.asnumpy(cp.angle(psi.zero_component)), cmap='jet')
+plt.show()
