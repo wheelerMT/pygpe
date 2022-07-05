c1d0ba242628893ab35d40857d2009251ef28a61
Matt Wheeler <mattwheeler15@hotmail.co.uk>
2022-07-05 19:40:41 +0100

    Added function to set the wavefunction array

--- pygpe/scalar/wavefunction.py
+++ pygpe/scalar/wavefunction.py
@@ -7,6 +7,14 @@ class Wavefunction:
     def __init__(self, grid: Grid):
         self.grid = grid
 
-        self.component = cp.empty(grid.shape, dtype='complex128')
+        self.wavefunction = cp.empty(grid.shape, dtype='complex128')
 
         self.atom_num = 0
+
+    def set_wavefunction(self, wavefunction: cp.ndarray) -> None:
+        """Sets the wavefunction to the specified state.
+
+        :param wavefunction:  The array to set the wavefunction as.
+        """
+        self.wavefunction = wavefunction
+       
