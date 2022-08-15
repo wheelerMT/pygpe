89792754d05f087a4f82c776c940a633cca45df5
Matt Wheeler <mattwheeler15@hotmail.co.uk>
2022-08-15 20:02:21 +0100

    Added re-normalise wavefunction function

--- pygpe/scalar/evolution.py
+++ pygpe/scalar/evolution.py
@@ -18,3 +18,24 @@ def potential_step(wfn: Wavefunction, pm: dict) -> None:
     :param pm: The parameter dictionary.
     """
     wfn.wavefunction *= cp.exp(-1j * pm["dt"] * (pm["g"] * cp.abs(wfn.wavefunction) ** 2))
+
+
+def renormalise_wavefunction(wfn: Wavefunction) -> None:
+    """Re-normalises the wavefunction to the correct atom number.
+
+    :param wfn: The wavefunction of the system.
+    """
+    wfn.ifft()
+    correct_atom_num = wfn.atom_num
+    current_atom_num = _calculate_atom_num(wfn)
+    wfn.wavefunction *= cp.sqrt(correct_atom_num / current_atom_num)
+    wfn.fft()
+
+
+def _calculate_atom_num(wfn: Wavefunction) -> float:
+    """Calculates the current atom number of the wavefunction.
+
+    :param wfn: The wavefunction of the system.
+    :return: The atom number.
+    """
+    return wfn.grid.grid_spacing_product * cp.sum(cp.abs(wfn.wavefunction) ** 2)
