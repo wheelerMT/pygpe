feefee28127e37bb50941f90481b6f1c4451b0ba
Matt Wheeler <mattwheeler15@hotmail.co.uk>
2022-09-07 19:36:02 +0100

    Updated conf.py to no longer include type hints in function signatures

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,6 +21,7 @@ sys.path.insert(0, os.path.abspath('../../'))
 
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.autosummary', 'sphinx_design']
 autosummary_generate = True
+autodoc_typehints = "none"
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
