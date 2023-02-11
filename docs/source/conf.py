e14bfc2c64457c8dcba8389a2f7c4a5ffeabd598
Matt Wheeler <wheelerMT@pm.me>
2023-02-11 16:41:20 +0000

    Updated version numbers

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,26 +1,26 @@
 import os
 import sys
 
-project = 'PyGPE'
-copyright = '2023, Matt Wheeler'
-author = 'Matt Wheeler'
-release = '1.1.1'
+project = "PyGPE"
+copyright = "2023, Matt Wheeler"
+author = "Matt Wheeler"
+release = "1.1.2"
 
-sys.path.insert(0, os.path.abspath('../../'))
+sys.path.insert(0, os.path.abspath("../../"))
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.autosummary', 'sphinx_design']
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.autosummary", "sphinx_design"]
 autosummary_generate = True
 autodoc_typehints = "none"
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'pydata_sphinx_theme'
-html_static_path = ['_static']
+html_theme = "pydata_sphinx_theme"
+html_static_path = ["_static"]
 html_css_files = ["pygpe.css"]
