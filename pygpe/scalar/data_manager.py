8264163ebd924161dbd9bfaf43cc4f904f058c4f
Matt Wheeler <wheelerMT@pm.me>
2023-03-20 20:15:53 +0000

    Refactored existing DataManager classes to use new abstract classes

--- pygpe/scalar/data_manager.py
+++ pygpe/scalar/data_manager.py
@@ -1,11 +1,11 @@
 import h5py
 import cupy as cp
-from pygpe.shared.grid import Grid
 from pygpe.shared import data_manager_paths as dmp
+from pygpe.shared.data_manager import _DataManager
 from pygpe.scalar.wavefunction import ScalarWavefunction
 
 
-class DataManager:
+class DataManager(_DataManager):
     """This object handles all the data of the simulation, including the
     wavefunction, grid, and parameter data.
 
@@ -18,54 +18,16 @@ class DataManager:
     :ivar data_path: The relative path to the folder containing the data file.
     """
 
-    def __init__(self, filename: str, data_path: str):
+    def __init__(
+        self,
+        filename: str,
+        data_path: str,
+        wfn: ScalarWavefunction,
+        params: dict,
+    ):
         """Constructs the DataManager object."""
-        self.filename = filename
-        self.data_path = data_path
-        self._time_index = 0
-
-        # Create file
-        h5py.File(f"{self.data_path}/{self.filename}", "w")
-
-    def save_initial_parameters(
-        self, grid: Grid, wfn: ScalarWavefunction, parameters: dict
-    ) -> None:
-        """Saves the initial grid, wavefunction and parameters to a HDF5 file.
-
-        :param grid: The grid object of the system.
-        :type grid: :class:`Grid`
-        :param wfn: The wavefunction of the system.
-        :type wfn: :class:`Wavefunction`
-        :param parameters: The parameter dictionary.
-        :type parameters: dict
-        """
-        if grid.ndim > 3 or grid.ndim < 1:
-            raise ValueError(
-                f"Grid with dimension of {grid.ndim} is unsupported, "
-                "please use a 1D, 2D, or 3D grid."
-            )
-        self._save_grid_params(grid)
+        super().__init__(filename, data_path, wfn, params)
         self._save_initial_wfn(wfn)
-        self._save_params(parameters)
-
-    def _save_grid_params(self, grid: Grid) -> None:
-        """Saves grid parameters to dataset."""
-        with h5py.File(f"{self.data_path}/{self.filename}", "r+") as file:
-            if grid.ndim == 1:
-                file.create_dataset(dmp.GRID_NX, data=grid.num_points_x)
-                file.create_dataset(dmp.GRID_DX, data=grid.grid_spacing_x)
-            elif grid.ndim == 2:
-                file.create_dataset(dmp.GRID_NX, data=grid.num_points_x)
-                file.create_dataset(dmp.GRID_NY, data=grid.num_points_y)
-                file.create_dataset(dmp.GRID_DX, data=grid.grid_spacing_x)
-                file.create_dataset(dmp.GRID_DY, data=grid.grid_spacing_y)
-            elif grid.ndim == 3:
-                file.create_dataset(dmp.GRID_NX, data=grid.num_points_x)
-                file.create_dataset(dmp.GRID_NY, data=grid.num_points_y)
-                file.create_dataset(dmp.GRID_NZ, data=grid.num_points_z)
-                file.create_dataset(dmp.GRID_DX, data=grid.grid_spacing_x)
-                file.create_dataset(dmp.GRID_DY, data=grid.grid_spacing_y)
-                file.create_dataset(dmp.GRID_DZ, data=grid.grid_spacing_z)
 
     def _save_initial_wfn(self, wfn: ScalarWavefunction) -> None:
         """Saves initial wavefunction to dataset."""
@@ -83,14 +45,6 @@ class DataManager:
                     (*wfn.grid.shape, 1),
                     maxshape=(*wfn.grid.shape, None),
                     dtype="complex128",
-                )
-
-    def _save_params(self, parameters: dict) -> None:
-        """Saves condensate parameters to dataset."""
-        with h5py.File(f"{self.data_path}/{self.filename}", "r+") as file:
-            for key in parameters:
-                file.create_dataset(
-                    f"{dmp.PARAMETERS}/{key}", data=parameters[key]
                 )
 
     def save_wavefunction(self, wfn: ScalarWavefunction) -> None:
